0538a9fac4365c8ba6eae013ca7a83a7652deb96
Vladislav Nagorny <u.slash.vlad@gmail.com>
2021-10-19 15:53:10 +0300

    TEMPORARY remove tests

--- test/api_tests/status.test.ts
+++
@@ -1,9 +0,0 @@
-import { axios } from '../utils';
-
-describe('/status', () => {
-  test('status is OK', async () => {
-    const response = await axios.get('/status');
-    expect(response.status).toBe(200);
-    expect(response.data).toStrictEqual({ text: 'OK!' });
-  });
-});
--- test/utils.ts
+++
@@ -1,8 +0,0 @@
-import { default as axios } from 'axios';
-export { default as axios } from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:8000';
-
-export function axiosStatusError(status: number) {
-  return 'Request failed with status code ' + status;
-}
